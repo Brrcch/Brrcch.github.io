6081609389679887f7fea04ad0615e4aa8c618d1
Bircch <matthewb0603@gmail.com>
2023-02-12 17:38:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// window.addEventListener("load", function() {
+//   if (window.innerWidth <= 1240) {
+//     var elements = document.getElementsByClassName("animate-up");
+//     for (var i = 0; i < elements.length; i++) {
+//       elements[i].classList.remove("animate-up");
+//     }
+//   }
+// });
